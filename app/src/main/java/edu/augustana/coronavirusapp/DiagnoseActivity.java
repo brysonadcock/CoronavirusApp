32a9ae4807d67acf712a504739fcb1a7126c5052
brysonadcock22 <brysonadcock16@augustana.edu>
2020-04-06 14:09:08 -0500

    Added symptoms and diagnose activities

---
+++ app/src/main/java/edu/augustana/coronavirusapp/DiagnoseActivity.java
@@ -0,0 +1,14 @@
+package edu.augustana.coronavirusapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiagnoseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagnose);
+    }
+}
