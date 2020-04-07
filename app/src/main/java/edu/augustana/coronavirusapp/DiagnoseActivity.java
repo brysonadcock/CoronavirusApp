4511c32965c0c16db94ae5f02f6d5f3f6ec618cc
brysonadcock22 <brysonadcock16@augustana.edu>
2020-04-06 23:07:25 -0500

    Added survey buttons to DiagnoseActivity

--- app/src/main/java/edu/augustana/coronavirusapp/DiagnoseActivity.java
+++ app/src/main/java/edu/augustana/coronavirusapp/DiagnoseActivity.java
@@ -3,6 +3,7 @@ package edu.augustana.coronavirusapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class DiagnoseActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class DiagnoseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnose);
+
+        Button feverBtn =findViewById(R.id.feverBtn);
+        Button coughBtn =findViewById(R.id.coughBtn);
+        Button shortBreathBtn =findViewById(R.id.shortBreathBtn);
+        Button muscleAcheBtn = findViewById(R.id.muscleAcheBtn);
+        Button soreThroatBtn =findViewById(R.id.soreThroatBtn);
+        Button Btn =findViewById(R.id.feverBtn);
     }
 }
--- app/src/main/java/edu/augustana/coronavirusapp/GuidelinesActivity.java
+++ app/src/main/java/edu/augustana/coronavirusapp/GuidelinesActivity.java
@@ -12,9 +12,11 @@ public class GuidelinesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guidelines);
 
+        // Sets the text in the two lists on the page
         TextView seekHelpView = findViewById(R.id.seekHelpView);
-        seekHelpView.setText("Seek Medical Attention if you exhibit the following:\n  •  Difficulty breathing\n  •  Persistent chest or pressure\n  •  Bluish Lips of Face\n");
         TextView tipsToStayHealthyList = findViewById(R.id.tipsToStayHealthyList);
-        tipsToStayHealthyList.setText("How To Protect Yourself:\n  •  Avoid close contact with others (6ft of space)\n  • Wash your hands often and after going in public\n  •  Cover your mouth and nose when around others\n  •  Cover coughs and sneezes\n  •  Clean and disinfect surfaces and phones daily\n");
+        seekHelpView.setText("Seek Medical Attention if you exhibit the following:\n  •  Difficulty breathing\n  •  Persistent chest or pressure\n  •  Bluish Lips of Face\n");
+        tipsToStayHealthyList.setText("How To Protect Yourself:\n  •  Avoid close contact with others (6ft of space)\n  • Wash your hands often and after going in public\n  •  Cover your mouth and nose when around others\n  •  Cover coughs and sneezes\n  •  Clean and disinfect surfaces and phones daily\n  •  Stay home unless getting food, medicine, or exercise\n");
     }
+
 }
